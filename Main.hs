bed1e9026e57c45ada1e3e0c94ed7cc07caa6532
drew gordon <drew.s.gordon@gmail.com>
2014-12-04 00:24:56 -0500

    connect mongodb and query all

--- Main.hs
+++ Main.hs
@@ -9,16 +9,17 @@ import qualified Text.Blaze.Html5 as B
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Text
 import Database.MongoDB
+import qualified Database.MongoDB as D
+import Control.Monad.IO.Class
+import qualified Data.Text.Lazy as T
 
-blaze = S.html . renderHtml
+runQuery :: Pipe -> D.Query -> IO [Document]
+runQuery pipe query = access pipe master "text_collections" (find query >>= rest)
 
-
-main = scotty 3002 $ do
-  get "/" $ do 
-   	S.html . renderHtml $ do
-   	  body $ do
-       h1 "heres a thing"
-       ul $ do
-         li "this"
-         li "or that"
-    
+main = do
+  pipe <- connect $ host "127.0.0.1"
+  scotty 3002 $ do
+    get "/" $ do
+      res <- liftIO $ runQuery pipe (D.select [] "yikyak")
+      text $ T.pack $ show res 
+   	
